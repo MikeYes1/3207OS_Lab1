d8f0f0880e2b7d76b5bb8964e2fb3e5ad8584c11
“Michael <tur87428@temple.edu>
2025-09-09 20:35:39 -0400

    pseudocodedraft

---
+++ tucp.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    /*
+    
+    FILE *fptr;
+    int i = 0;
+    
+    if(status(argv[argc-2]) is directory){
+    
+        while( i != length(argv) - 2 ){
+      
+            fptr = fopen(argv[i], r) 
+                find out if dynamic string arrays don't use [ ]
+    
+            fscanf and write the contents
+             
+            fclose(fptr)
+             
+            fptr = fopen(argv[argc-2], w)
+            
+            fputs(*fptr, copy)
+             
+            fclose(fptr)
+        
+      
+        i++
+        }
+    } else {
+    
+        fptr = fopen(argv[0], r)
+        
+        copy somewhere
+        
+        fclose(fptr)
+         
+        fptr = fopen(argv[1], w)
+            
+        fputs(*fptr, copy)
+         
+        fclose(fptr)
+
+    */
+}
